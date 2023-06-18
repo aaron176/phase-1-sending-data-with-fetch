dbc61b7d3cb22273e47ca53956e4b1b4dfe381c7
haroon <haroon.seena2017@gmail.com>
2023-06-18 15:54:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+const info = {
+    name: 'haroon',
+    age: '20'
+}
+
+const submitData = {
+ method: "POST",
+ Headers : {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+ },
+ body:JSON.stringify(info),
+};
+
+fetch("http://localhost:3000/users",submitData)
+.then(res => res.json())
+.then(data => document.body.innerHTML = data["id"])
+.catch(error => document.body.innerHTML = error.message );
